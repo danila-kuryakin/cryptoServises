9c0f0f87436a4e7c1d44d6da1d4ab1af9cc396c8
danila <danila.kuryakin@gmail.com>
2026-01-18 14:29:08 +0300

    Изменения в индексере. Добавил spaces

--- governance-indexer/internal/repository/repository.go
+++ governance-indexer/internal/repository/repository.go
@@ -10,12 +10,21 @@ type ProposalRepo interface {
 	FindMissing(proposals []models.Proposals) ([]models.Proposals, error)
 }
 
+type SpaceRepo interface {
+	Add(space []models.Space, eventType string) error
+	AddHistory(space []models.Space) error
+	AddNew(space []models.Space) error
+	FindMissing(spaces []models.Space) ([]models.Space, error)
+}
+
 type Repository struct {
 	ProposalRepo
+	SpaceRepo
 }
 
 func NewRepository(db *sql.DB) *Repository {
 	return &Repository{
 		ProposalRepo: NewProposalPostgres(db),
+		SpaceRepo:    NewSpacePostgres(db),
 	}
 }
